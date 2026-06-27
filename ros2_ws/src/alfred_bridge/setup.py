3768b0c4c06a0ceee875fed13cad91817cddf4c3
AstroGear Labs <73603820+Robotics1977@users.noreply.github.com>
2026-06-27 11:48:52 -0500

    Update setup.py

--- ros2_ws/src/alfred_bridge/setup.py
+++ ros2_ws/src/alfred_bridge/setup.py
@@ -1,1 +1,26 @@
+from setuptools import find_packages, setup
 
+package_name = 'alfred_bridge'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='alfred',
+    maintainer_email='alfred@todo.todo',
+    description='Alfred 1 serial bridge node',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'alfred_bridge_node = alfred_bridge.alfred_bridge_node:main',
+        ],
+    },
+)
